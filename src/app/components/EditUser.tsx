91c180845aafe1d172add2642a4bd8d691ad94d1
Rafael <rhmcord@gmail.com>
2025-06-02 10:35:48 -0500

    chore: improve user edition form

--- src/app/components/EditUser.tsx
+++ src/app/components/EditUser.tsx
@@ -192,15 +192,14 @@ export default function EditUser({
                     htmlFor="first_name"
                     className="inline-block mb-2 text-sm"
                   >
-                    Nombre
+                    First name
                   </label>
                   <input
                     type="text"
                     id="first_name"
-                    {...register("first_name")}
                     required
                     disabled
-                    className="w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+                    className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
                   />
                 </div>
                 <div>
@@ -208,15 +207,14 @@ export default function EditUser({
                     htmlFor="last_name"
                     className="inline-block mb-2 text-sm"
                   >
-                    Apellido
+                    Last name
                   </label>
                   <input
                     type="text"
                     id="name"
-                    {...register("last_name")}
                     required
                     disabled
-                    className="w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+                    className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
                   />
                 </div>
                 <div>
@@ -230,9 +228,8 @@ export default function EditUser({
                     disabled
                     type="text"
                     id="username"
-                    {...register("username")}
                     required
-                    className="w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+                    className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
                   />
                 </div>
                 <div>
@@ -243,9 +240,8 @@ export default function EditUser({
                     disabled
                     type="email"
                     id="email"
-                    {...register("email")}
                     required
-                    className="w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+                    className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
                   />
                 </div>
               </fieldset>

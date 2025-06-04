e61695f8d16542c5cba1aef9fce0e4de0e7d0b8f
Rafael <rhmcord@gmail.com>
2025-06-04 08:43:04 -0500

    feat: remove footer from form

--- src/app/components/EditTemplate.tsx
+++ src/app/components/EditTemplate.tsx
@@ -86,6 +86,38 @@ export default function EditTemplate({ id }: { id: string }) {
           <form onSubmit={handleSubmit(onSubmit)}>
             <fieldset className="flex flex-col gap-4">
               <div className="flex p-7 flex-col gap-4 border border-gray-100 rounded-xl bg-white">
+                <h2 className="font-semibold">General Information</h2>
+                <div className="flex gap-4 items-center">
+                  <div className="grow-1">
+                    <label htmlFor="name" className="inline-block mb-2 text-sm">
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      {...register("name")}
+                      required
+                      className="w-full px-4 bg-white py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+                    />
+                  </div>
+                  <div className="grow-1">
+                    <label
+                      htmlFor="description"
+                      className="inline-block mb-2 text-sm"
+                    >
+                      Description
+                    </label>
+                    <input
+                      type="text"
+                      id="description"
+                      {...register("description")}
+                      required
+                      className="w-full bg-white px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex p-7 flex-col gap-4 border border-gray-100 rounded-xl bg-white">
                 <h2 className="font-semibold">
                   Header Image <br />
                 </h2>
@@ -132,51 +164,7 @@ export default function EditTemplate({ id }: { id: string }) {
                   />
                 </fieldset>
               </div>
-              <div className="flex p-7 flex-col gap-4 border border-gray-100 rounded-xl bg-white">
-                <h2 className="font-semibold">General Information</h2>
-                <div>
-                  <label htmlFor="name" className="inline-block mb-2 text-sm">
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    {...register("name")}
-                    required
-                    className="w-full px-4 bg-white py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="description"
-                    className="inline-block mb-2 text-sm"
-                  >
-                    Description
-                  </label>
-                  <input
-                    type="text"
-                    id="description"
-                    {...register("description")}
-                    required
-                    className="w-full bg-white px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
-                  />
-                </div>
-              </div>
             </fieldset>
-            <footer className="flex items-center gap-3.5 justify-end mt-6 pt-6">
-              <Link
-                href="/admin/templates"
-                className="font-semibold  disabled:border-gray-100 disabled:bg-gray-100 inline-block py-3 px-10 bg-white text-sm border border-gray-100 rounded-lg transition-colors hover:border-gray-200 duration-500 active:border-gray-300"
-              >
-                Cancel
-              </Link>
-              <button
-                type="submit"
-                className="text-white font-semibold disabled:border-gray-100 disabled:bg-gray-100 inline-block py-3 px-10 text-sm bg-cyan-500 hover:bg-cyan-400 transition-colors duration-500 rounded-lg"
-              >
-                Save
-              </button>
-            </footer>
           </form>
         </>
       )}

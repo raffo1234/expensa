0329a9fc0283675e3738af64d42a3f2529f4acad
Rafael <rhmcord@gmail.com>
2025-06-06 13:39:43 -0500

    feat: remove form in favor of update by field

--- src/app/components/EditUserContent.tsx
+++ src/app/components/EditUserContent.tsx
@@ -3,7 +3,7 @@
 import { Permissions } from "@/types/propertyState";
 import { supabase } from "@/lib/supabase";
 import FieldsSection from "./FieldsSection";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useReducer, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import useSWR from "swr";
 import { UUIDTypes } from "uuid";
@@ -11,6 +11,7 @@ import useCheckPermission from "@/hooks/useCheckPermission";
 import { adminRolesKey } from "@/constants";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import ResidentItem from "./ResidentItem";
+import toast from "react-hot-toast";
 
 async function fetcher(userId: string) {
   const { data, error } = await supabase
@@ -101,38 +102,21 @@ export default function EditUserContent({
 
   const { data: user } = useSWR(`admin-${userId}`, () => fetcher(userId));
 
-  const { reset, register, handleSubmit } = useForm<Inputs>({
-    mode: "onBlur",
-    defaultValues: useMemo(() => {
-      return user;
-    }, [user]),
-  });
-
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    if (data.template_id === "") {
-      data.template_id = null;
-    }
-
+  const updateUser = async (fieldName: string, data: string) => {
     setIsSaving(true);
-
     try {
-      const { data: updatedUser } = await supabase
+      const { error } = await supabase
         .from("user")
-        .update(data)
-        .eq("id", userId)
-        .select()
-        .single();
-      if (updatedUser) console.log("success!!!");
+        .update({ [fieldName]: data })
+        .eq("id", userId);
+
+      if (!error) toast.success("User Updated successfully!");
     } catch (error) {
       console.error(error);
     } finally {
       setIsSaving(false);
     }
   };
-
-  useEffect(() => {
-    reset(user);
-  }, [user]);
 
   if (
     isLoadingUsers ||
@@ -146,151 +130,141 @@ export default function EditUserContent({
   return (
     <>
       <h2 className="mb-6 font-semibold text-lg block">Edit User</h2>
-      <form onSubmit={handleSubmit(onSubmit)} id="editUser">
-        <fieldset className="flex flex-col gap-4">
-          <FieldsSection>
-            <h2 className="font-semibold">Role and Location</h2>
-            <div className="flex items-center gap-4 w-full">
-              <div className="flex-grow">
-                <label htmlFor="role_id" className="inline-block mb-2 text-sm">
-                  Role
-                </label>
-                <div className="relative">
-                  <select
-                    id="role_id"
-                    {...register("role_id")}
-                    className="w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500 bg-white"
-                  >
-                    {roles?.map(({ id, name }) => {
-                      return (
-                        <option value={id} key={id}>
-                          {name}
-                        </option>
-                      );
-                    })}
-                  </select>
-                  <div className="absolute top-1/2 -translate-y-1/2 right-1 pr-3 pointer-events-none bg-white">
-                    <Icon icon="solar:alt-arrow-down-linear" fontSize={16} />
-                  </div>
-                </div>
-              </div>
-              <div className="flex-grow">
-                <label
-                  htmlFor="template_id"
-                  className="inline-block mb-2 text-sm"
+
+      <fieldset className="flex flex-col gap-4">
+        <FieldsSection>
+          <h2 className="font-semibold">Role and Location</h2>
+          <div className="flex items-center gap-4 w-full">
+            <div className="flex-grow">
+              <label htmlFor="role_id" className="inline-block mb-2 text-sm">
+                Role
+              </label>
+              <div className="relative">
+                <select
+                  id="role_id"
+                  defaultValue={user.role_id}
+                  onChange={(event) =>
+                    updateUser("role_id", event.target.value)
+                  }
+                  className="w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500 bg-white"
                 >
-                  Locations
-                </label>
-                <div className="relative">
-                  <select
-                    id="template_id"
-                    {...register("template_id")}
-                    className="w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500 bg-white"
-                  >
-                    <option value="">Select ...</option>
-                    {templates?.map(({ id, name }) => {
-                      return (
-                        <option value={id} key={id}>
-                          {name}
-                        </option>
-                      );
-                    })}
-                  </select>
-                  <div className="absolute top-1/2 -translate-y-1/2 right-1 pr-3 pointer-events-none bg-white">
-                    <Icon icon="solar:alt-arrow-down-linear" fontSize={16} />
-                  </div>
+                  {roles?.map(({ id, name }) => {
+                    return (
+                      <option value={id} key={id}>
+                        {name}
+                      </option>
+                    );
+                  })}
+                </select>
+                <div className="absolute top-1/2 -translate-y-1/2 right-1 pr-3 pointer-events-none bg-white">
+                  <Icon icon="solar:alt-arrow-down-linear" fontSize={16} />
                 </div>
               </div>
             </div>
-          </FieldsSection>
-          {canHaveResident ? (
-            <FieldsSection>
-              <h2 className="font-semibold">Residents</h2>
-              <div className="flex gap-1 items-center">
-                {users?.map((user) => {
-                  return user.role_id ? (
-                    <ResidentItem
-                      isEditable={canAssignResident}
-                      key={user.id}
-                      user={user}
-                    />
-                  ) : null;
-                })}
+            <div className="flex-grow">
+              <label
+                htmlFor="template_id"
+                className="inline-block mb-2 text-sm"
+              >
+                Locations
+              </label>
+              <div className="relative">
+                <select
+                  id="template_id"
+                  onChange={(event) =>
+                    updateUser("template_id", event.target.value)
+                  }
+                  defaultValue={user.template_id}
+                  className="w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500 bg-white"
+                >
+                  <option value="">Select ...</option>
+                  {templates?.map(({ id, name }) => {
+                    return (
+                      <option value={id} key={id}>
+                        {name}
+                      </option>
+                    );
+                  })}
+                </select>
+                <div className="absolute top-1/2 -translate-y-1/2 right-1 pr-3 pointer-events-none bg-white">
+                  <Icon icon="solar:alt-arrow-down-linear" fontSize={16} />
+                </div>
               </div>
-            </FieldsSection>
-          ) : null}
+            </div>
+          </div>
+        </FieldsSection>
+        {canHaveResident ? (
           <FieldsSection>
-            <h2 className="font-semibold">General Information</h2>
-            <div>
-              <label htmlFor="first_name" className="inline-block mb-2 text-sm">
-                First name
-              </label>
-              <input
-                {...register("first_name")}
-                type="text"
-                id="first_name"
-                required
-                disabled
-                className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
-              />
-            </div>
-            <div>
-              <label htmlFor="last_name" className="inline-block mb-2 text-sm">
-                Last name
-              </label>
-              <input
-                {...register("last_name")}
-                type="text"
-                id="name"
-                required
-                disabled
-                className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
-              />
-            </div>
-            <div>
-              <label htmlFor="username" className="inline-block mb-2 text-sm">
-                Username
-              </label>
-              <input
-                {...register("username")}
-                disabled
-                type="text"
-                id="username"
-                required
-                className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
-              />
-            </div>
-            <div>
-              <label htmlFor="email" className="inline-block mb-2 text-sm">
-                Email
-              </label>
-              <input
-                {...register("email")}
-                disabled
-                type="email"
-                id="email"
-                required
-                className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
-              />
+            <h2 className="font-semibold">Residents</h2>
+            <div className="flex gap-1 items-center">
+              {users?.map((user) => {
+                return user.role_id ? (
+                  <ResidentItem
+                    isEditable={canAssignResident}
+                    key={user.id}
+                    user={user}
+                  />
+                ) : null;
+              })}
             </div>
           </FieldsSection>
-        </fieldset>
-        <footer className="flex items-center gap-3.5 justify-end mt-6 pt-6">
-          <button
-            type="button"
-            className="font-semibold disabled:border-gray-100 disabled:bg-gray-100 inline-block py-3 px-10 bg-white text-sm border border-gray-100 rounded-lg transition-colors hover:border-gray-200 duration-500 active:border-gray-300"
-          >
-            Cancel
-          </button>
-          <button
-            disabled={isSaving}
-            type="submit"
-            className="text-white font-semibold disabled:opacity-80 inline-block py-3 px-10 text-sm bg-cyan-500 hover:bg-cyan-400 transition-colors duration-500 rounded-lg"
-          >
-            {isSaving ? "Saving..." : "Save"}
-          </button>
-        </footer>
-      </form>
+        ) : null}
+        <FieldsSection>
+          <h2 className="font-semibold">General Information</h2>
+          <div>
+            <label htmlFor="first_name" className="inline-block mb-2 text-sm">
+              First name
+            </label>
+            <input
+              defaultValue={user.first_name}
+              type="text"
+              id="first_name"
+              required
+              disabled
+              className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+            />
+          </div>
+          <div>
+            <label htmlFor="last_name" className="inline-block mb-2 text-sm">
+              Last name
+            </label>
+            <input
+              defaultValue={user.last_name}
+              type="text"
+              id="name"
+              required
+              disabled
+              className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+            />
+          </div>
+          <div>
+            <label htmlFor="username" className="inline-block mb-2 text-sm">
+              Username
+            </label>
+            <input
+              defaultValue={user.username}
+              disabled
+              type="text"
+              id="username"
+              required
+              className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+            />
+          </div>
+          <div>
+            <label htmlFor="email" className="inline-block mb-2 text-sm">
+              Email
+            </label>
+            <input
+              defaultValue={user.email}
+              disabled
+              type="email"
+              id="email"
+              required
+              className="disabled:bg-gray-50 disabled:text-gray-500 w-full px-4 py-2.5 rounded-xl border border-gray-200 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+            />
+          </div>
+        </FieldsSection>
+      </fieldset>
     </>
   );
 }

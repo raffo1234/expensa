b7288b8a2c979e98f9c23f3415528d9f1333890c
Rafael <rhmcord@gmail.com>
2025-06-09 23:37:39 -0500

    fix: styles

--- src/app/components/Pac.tsx
+++ src/app/components/Pac.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PacType } from "@/types/PacType";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import DeletePac from "./DeletePac";
@@ -5,8 +7,10 @@ import { useDebouncedCallback } from "use-debounce";
 import { supabase } from "@/lib/supabase";
 import { mutate } from "swr";
 import toast from "react-hot-toast";
+import { useState } from "react";
 
 export default function Pac({ pac }: { pac: PacType }) {
+  const [isOpen, setIsOpen] = useState(false);
   const { id, ip, aet_server, aet_client, port } = pac;
 
   const updatePac = async (id: string, newData: Partial<PacType>) => {
@@ -32,57 +36,60 @@ export default function Pac({ pac }: { pac: PacType }) {
       >
         <button
           type="button"
-          className="flex-col border-b border-gray-200 w-full items-start gap-1 flex md:flex-row sm:gap-3.5 md:items-center py-4 pl-6 pr-20"
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="flex-col cursor-pointer hover:bg-gray-50 first:rounded-t-xl w-full items-start gap-1 flex md:flex-row sm:gap-3.5 md:items-center py-4 pl-6 pr-20"
         >
           <Icon
             icon="solar:alt-arrow-down-linear"
             fontSize={20}
             className={`
-                            ${true ? "rotate-180" : ""} 
+                            ${isOpen ? "rotate-180" : ""} 
                           transition-transform duration-500 flex-shrink-0`}
           />
           <span>{aet_server}</span>
         </button>
         <DeletePac pacId={id} />
       </div>
-      <div className="px-12 py-10">
-        <div className="flex-col items-start gap-1 flex md:flex-row sm:gap-3.5 md:items-center">
-          <div className="flex gap-3.5 items-center">
+      {isOpen ? (
+        <div className="px-12 py-10 border-t border-gray-200">
+          <div className="flex-col items-start gap-1 flex md:flex-row sm:gap-3.5 md:items-center">
+            <div className="flex gap-3.5 items-center">
+              <input
+                onChange={(event) =>
+                  debouncedUpdate(id, { ip: event.target.value })
+                }
+                placeholder="IP"
+                defaultValue={ip}
+                className="w-33 p-2 border border-transparent rounded-xl focus:outline-1 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+              />
+              <input
+                placeholder="Port"
+                defaultValue={port}
+                className="w-13 text-sm text-gray-500 p-2 border border-transparent rounded-xl focus:outline-1 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+                onChange={(event) =>
+                  debouncedUpdate(id, { port: event.target.value })
+                }
+              />
+            </div>
             <input
+              placeholder="AET Server"
+              defaultValue={aet_server}
               onChange={(event) =>
-                debouncedUpdate(id, { ip: event.target.value })
+                debouncedUpdate(id, { aet_server: event.target.value })
               }
-              placeholder="IP"
-              defaultValue={ip}
-              className="w-33 p-2 border border-transparent rounded-xl focus:outline-1 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
+              className="text-sm  text-gray-500 p-2 border border-transparent rounded-xl focus:outline-1 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
             />
             <input
-              placeholder="Port"
-              defaultValue={port}
-              className="w-13 text-sm text-gray-500 p-2 border border-transparent rounded-xl focus:outline-1 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
               onChange={(event) =>
-                debouncedUpdate(id, { port: event.target.value })
+                debouncedUpdate(id, { aet_client: event.target.value })
               }
+              placeholder="AET Client (Optional)"
+              defaultValue={aet_client}
+              className="text-sm  text-gray-500 p-2 border border-transparent rounded-xl focus:outline-1 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
             />
           </div>
-          <input
-            placeholder="AET Server"
-            defaultValue={aet_server}
-            onChange={(event) =>
-              debouncedUpdate(id, { aet_server: event.target.value })
-            }
-            className="text-sm  text-gray-500 p-2 border border-transparent rounded-xl focus:outline-1 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
-          />
-          <input
-            onChange={(event) =>
-              debouncedUpdate(id, { aet_client: event.target.value })
-            }
-            placeholder="AET Client (Optional)"
-            defaultValue={aet_client}
-            className="text-sm  text-gray-500 p-2 border border-transparent rounded-xl focus:outline-1 focus:outline-none focus:ring-4 focus:ring-cyan-100  focus:border-cyan-500"
-          />
         </div>
-      </div>
+      ) : null}
     </>
   );
 }

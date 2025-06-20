dc96f3904bf87e55ac8b67d8587d990ff9523dbf
Rafael <rhmcord@gmail.com>
2025-06-19 21:43:06 -0500

    fix: expire verification

--- src/app/view/pdf/[id]/page.tsx
+++ src/app/view/pdf/[id]/page.tsx
@@ -4,13 +4,6 @@ import { SharedLinkType } from "@/types/sharedLinkType";
 
 type Params = Promise<{ id: string }>;
 
-function hasExpired(expirationTimestamp: Date): boolean {
-  const expiryDate = new Date(expirationTimestamp);
-  const now = new Date();
-
-  return expiryDate.getTime() < now.getTime();
-}
-
 export default async function Page({ params }: { params: Params }) {
   const { id } = await params;
 
@@ -18,15 +11,11 @@ export default async function Page({ params }: { params: Params }) {
     .from("shared_link")
     .select("expire_at, dicom_id, dicom(*, template(*))")
     .eq("id", id)
+    .gt("expire_at", new Date().toISOString())
     .single()) as { data: SharedLinkType | null };
 
   if (!data) return null;
 
-  if (!hasExpired(data.expire_at)) {
-    console.log("Not expire yet:", data.expire_at);
-  }
-
   if (!data.dicom) return null;
-
   return <PDFPreview dicom={data.dicom} />;
 }

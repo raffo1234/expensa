6a201042dbf2e01756f4f7ec8089f57bfaec3b61
Rafael <rhmcord@gmail.com>
2025-06-20 09:41:30 -0500

    feat: improve styles

--- src/app/components/ShareReport.tsx
+++ src/app/components/ShareReport.tsx
@@ -107,36 +107,27 @@ export default function ShareReport({
           );
         })}
       </div>
-      <fieldset className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <FieldsSection>
           <h2 className="font-semibold">Direct link</h2>
-          <div className="flex gap-4 items-center">
-            <div className="grow-1 relative">
-              <FieldLabel htmlFor="direct-link">
-                Anyone with the link can access
-              </FieldLabel>
-              <div className="relative">
-                <input
-                  readOnly
-                  disabled
-                  title={`${process.env.NEXT_PUBLIC_URL}/view/pdf/${sharedLinkId}`}
-                  type="text"
-                  id="direct-link"
-                  value={`${process.env.NEXT_PUBLIC_URL}/view/pdf/${sharedLinkId}`}
-                  required
-                  className="text-gray-500 w-full px-4 bg-white py-2.5 rounded-xl border border-gray-200 focus:outline-none"
-                />
-                <button
-                  onClick={() =>
-                    copyLink(
-                      `${process.env.NEXT_PUBLIC_URL}/view/pdf/${sharedLinkId}`
-                    )
-                  }
-                  className="hover:bg-cyan-50 hover:border-cyan-200 cursor-pointer transition-colors duration-300 absolute right-1 top-1/2 -translate-y-1/2 text-sm bg-gray-50 border px-5 py-2 border-gray-200 rounded-lg"
-                >
-                  Copy link
-                </button>
+          <div>
+            <div className="inline-block mb-2 text-sm">
+              Anyone with the link can access
+            </div>
+            <div className="relative w-full">
+              <div className="truncate text-gray-500 pr-30 w-full px-4 bg-white py-2.5 rounded-xl border border-gray-200">
+                {`${process.env.NEXT_PUBLIC_URL}/view/pdf/${sharedLinkId}`}
               </div>
+              <button
+                onClick={() =>
+                  copyLink(
+                    `${process.env.NEXT_PUBLIC_URL}/view/pdf/${sharedLinkId}`
+                  )
+                }
+                className="hover:bg-cyan-50 hover:border-cyan-200 cursor-pointer transition-colors duration-300 absolute right-1 top-1/2 -translate-y-1/2 text-sm bg-gray-50 border px-5 py-2 border-gray-200 rounded-lg"
+              >
+                Copy link
+              </button>
             </div>
           </div>
         </FieldsSection>
@@ -162,7 +153,7 @@ export default function ShareReport({
             </div>
           </div>
         </FieldsSection>
-      </fieldset>
+      </div>
     </>
   );
 }

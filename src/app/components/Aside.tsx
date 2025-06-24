3626a0aebe49fde7bc16452fdeb9a5dd1600a1a3
Rafael <rhmcord@gmail.com>
2025-06-23 19:02:50 -0500

    feat: restore scrollbar

--- src/app/components/Aside.tsx
+++ src/app/components/Aside.tsx
@@ -21,13 +21,13 @@ export default function Aside({
 
   const closeMenu = () => {
     setIsOpen(false);
+    const documentElement = document.documentElement;
+    documentElement.style.overflow = "visible";
   };
 
   const handleToggle = () => {
     if (isOpen) {
       closeMenu();
-      const documentElement = document.documentElement;
-      documentElement.style.overflow = "auto";
     } else {
       handleOpen();
     }

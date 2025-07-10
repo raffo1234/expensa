8ca726902fcae3cb1da340ddc789fa41e727d123
raffo1234 <rhmcord@gmail.com>
2025-07-10 15:41:41 -0500

    fix: user role

--- src/app/(admin)/admin/dicom/page.tsx
+++ src/app/(admin)/admin/dicom/page.tsx
@@ -7,18 +7,13 @@ export default async function Page() {
   const session = await auth();
   const user = session?.user;
 
-  if (user?.id) {
-    const { data } = await supabase
-      .from("user")
-      .select("role_id, template_id")
-      .eq("id", user?.id)
-      .single();
+  const { data } = await supabase
+    .from("user")
+    .select("role_id, template_id")
+    .eq("id", user?.id)
+    .single();
 
-    user.role_id = data?.role_id;
-    user.template_id = data?.template_id;
-  }
-
-  if (!user?.id || !user?.email || !user.role_id) return null;
+  if (!user?.id || !user?.email || !data?.role_id) return null;
 
   return (
     <>
@@ -29,13 +24,9 @@ export default async function Page() {
             .dcm Compressed, Folder containing .dcm or a .dcm
           </span>
         </h1>
-        <ViewAllDicomsLink userRoleId={user.role_id} />
+        <ViewAllDicomsLink userRoleId={data.role_id} />
       </div>
-      <UploaderPage
-        userEmail={user.email}
-        userId={user.id}
-        userRoleId={user.role_id}
-      />
+      <UploaderPage userEmail={user.email} userId={user.id} userRoleId={data.role_id} />
     </>
   );
 }

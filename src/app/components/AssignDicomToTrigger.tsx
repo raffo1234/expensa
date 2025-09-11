e68ab3115ad750990cf12561c79261fdfc305c47
Rafael Meza <ralphch1234562@hotmail.com>
2025-09-11 18:44:25 -0500

    fix: markup

--- src/app/components/AssignDicomToTrigger.tsx
+++ src/app/components/AssignDicomToTrigger.tsx
@@ -15,7 +15,7 @@ function AssignmentIcon() {
           strokeLinecap="round"
           d="M21 12.001v-4c0-2.358 0-3.536-.732-4.269C19.535 3 18.357 3 16 3h-3c-2.357 0-3.536 0-4.268.732C8 4.465 8 5.643 8 8.001v8c0 2.358 0 3.537.732 4.27c.62.62 1.561.714 3.268.729m0-14h5m-5 4h5"
         />
-        <path stroke-linecap="round" d="M14 19s1.5.5 2.5 2c0 0 1.5-4 5.5-6M2 7h3" />
+        <path strokeLinecap="round" d="M14 19s1.5.5 2.5 2c0 0 1.5-4 5.5-6M2 7h3" />
       </g>
     </svg>
   );

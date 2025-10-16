115bc3c1a7342cfb7ecee76e615bdef6545d262c
raffo1234 <rhmcord@gmail.com>
2025-10-16 17:18:38 -0500

    fix: typo patient email

--- src/app/components/UploadEmailUser.tsx
+++ src/app/components/UploadEmailUser.tsx
@@ -18,10 +18,7 @@ export default function UploadEmailUser({ link }: { link: string }) {
       <Tailwind>
         <Body>
           <Container>
-            <Preview>
-              Instant access to your Cadia study report - download or view
-              online.
-            </Preview>
+            <Preview>Instant access to your Cadia study report - download or view online.</Preview>
             <Section className="my-[16px]">
               <Section className="text-center">
                 <Button
@@ -49,17 +46,17 @@ export default function UploadEmailUser({ link }: { link: string }) {
                   target="_blank"
                   className="my-[16px] font-semibold text-[18px] text-rose-400 leading-[28px]"
                 >
-                  Cadia
+                  Cadia.pe
                 </Link>
                 <Heading
                   as="h1"
                   className="m-0 mt-[8px] font-semibold text-[36px] text-gray-900 leading-[36px]"
                 >
-                  A Report was Uploaded
+                  Your scan was Uploaded
                 </Heading>
                 <Text className="text-[16px] text-gray-500 leading-[24px]">
-                  We are happy to let you know that your latest report has been
-                  successfully uploaded and is now instantly accessible.
+                  We are happy to let you know that your latest report has been successfully
+                  uploaded and is now instantly accessible.
                 </Text>
                 <Button
                   className="text-[16px] cursor-pointer text-white font-semibold mb-8 gap-4 px-8 py-4 bg-cyan-500 rounded-full"
@@ -68,9 +65,8 @@ export default function UploadEmailUser({ link }: { link: string }) {
                   View The Report
                 </Button>
                 <Text className="text-[14px] text-gray-500 leading-[24px]">
-                  For more detailed information about our services, including
-                  the types of studies we offer and how we can help you, please
-                  visit our website at{" "}
+                  For more detailed information about our services, including the types of studies
+                  we offer and how we can help you, please visit our website at{" "}
                   <Link
                     href="https://www.cadia.pe/"
                     target="_blank"

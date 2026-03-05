ba69586e1839ef9d98baf0006ed5e631bb7919e6
raffo1234 <rhmcord@gmail.com>
2026-03-05 16:01:35 -0500

    feat: reuse inner button on clear one

--- src/app/components/ClearButton.tsx
+++ src/app/components/ClearButton.tsx
@@ -4,12 +4,13 @@ import { ICON_SIZE } from "@/constants";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useState } from "react";
 import { Popover } from "react-tiny-popover";
+import InnerCircularButton from "./InnerCircularButton";
 
 export default function ClearButton({ clearLocalStorage }: { clearLocalStorage: () => void }) {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const openPopover = () => setIsPopoverOpen(true);
   const closePopover = () => setIsPopoverOpen(false);
-
+  const title = "Clear all filters";
   return (
     <Popover
       isOpen={true}
@@ -17,8 +18,7 @@ export default function ClearButton({ clearLocalStorage }: { clearLocalStorage: 
       padding={12}
       content={
         <div
-          className={`${isPopoverOpen ? "opacity-100 -translate-y-0" : "opacity-0 -translate-y-4"} 
-              pointer-events-none text-white px-3 py-2 max-w-48 bg-slate-800 rounded-lg transition-all duration-500 ease-in-out`}
+          className={`${isPopoverOpen ? "opacity-100 -translate-y-0" : "opacity-0 -translate-y-4"} pointer-events-none text-white px-3 py-2 max-w-48 bg-slate-800 rounded-lg transition-all duration-500 ease-in-out`}
         >
           Clear all filters
         </div>
@@ -27,11 +27,12 @@ export default function ClearButton({ clearLocalStorage }: { clearLocalStorage: 
       <button
         onMouseEnter={openPopover}
         onMouseLeave={closePopover}
-        title="Clear all filters"
         onClick={clearLocalStorage}
-        className="cursor-pointer text-cyan-400 hover:text-cyan-600 transition-colors duration-300 p-2 border border-cyan-100 rounded-full"
+        title={title}
       >
-        <Icon icon="pajamas:clear-all" fontSize={ICON_SIZE}></Icon>
+        <InnerCircularButton title={title}>
+          <Icon icon="pajamas:clear-all" fontSize={ICON_SIZE}></Icon>
+        </InnerCircularButton>
       </button>
     </Popover>
   );

dddb825dafe47be9f7e1592deabfcd9893cf7e81
raffo1234 <rhmcord@gmail.com>
2026-03-17 17:28:38 -0500

    feat: update alert message

--- src/app/(admin)/admin/upload/page.tsx
+++ src/app/(admin)/admin/upload/page.tsx
@@ -16,9 +16,7 @@ export default async function Page() {
     <>
       <div className="flex justify-between">
         <h1 className="mb-6 font-semibold text-lg block">
-          <strong className="text-rose-400 font-semibold">TEST!!!</strong><br />
-          {t("title")}
-
+          {t("title")} <strong className="text-rose-400 font-semibold">READY FOR TESTING!</strong>
           <span className="text-sm text-gray-500 font-normal block pt-1">{t("description")}</span>
         </h1>
         <Suspense>

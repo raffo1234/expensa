6e452cb614b6afb86e74e68de72834355d5848c9
raffo1234 <rhmcord@gmail.com>
2026-03-23 12:14:46 -0500

    chore: move title outside of suspense statement

--- src/app/(admin)/admin/templates/[id]/page.tsx
+++ src/app/(admin)/admin/templates/[id]/page.tsx
@@ -2,6 +2,7 @@ import { supabase } from "@/lib/supabase";
 import EditTemplate from "@/components/EditTemplate";
 import { Suspense } from "react";
 import FormSkeleton from "@/components/FormSkeleton";
+import Link from "next/link";
 
 type Params = Promise<{ id: string }>;
 
@@ -9,15 +10,33 @@ export default async function Page({ params }: { params: Params }) {
   const { id } = await params;
   if (!id) return null;
 
-  const { data: template } = await supabase
-    .from("template")
-    .select("*")
-    .eq("id", id)
-    .single();
+  const { data: template } = await supabase.from("template").select("*").eq("id", id).maybeSingle();
 
   return (
-    <Suspense fallback={<FormSkeleton rows={2} />}>
-      <EditTemplate id={id} fallbackTemplate={template ?? undefined} />
-    </Suspense>
+    <>
+      <div className="flex mb-3 items-center justify-between -mt-3">
+        <h1 className="font-semibold text-lg block">
+          <span className="capitalize">{template.name}</span>
+        </h1>
+        <Link
+          href="/admin/templates"
+          title="Templates"
+          className="p-2 hover:text-cyan-400 transition-colors duration-300"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+            <g fill="none" stroke="currentColor" strokeWidth="1.5">
+              <path
+                d="M11.142 20c-2.227 0-3.341 0-4.27-.501c-.93-.502-1.52-1.42-2.701-3.259l-.681-1.06C2.497 13.634 2 12.86 2 12s.497-1.634 1.49-3.18l.68-1.06c1.181-1.838 1.771-2.757 2.701-3.259S8.915 4 11.142 4h2.637c3.875 0 5.813 0 7.017 1.172S22 8.229 22 12s0 5.657-1.204 6.828S17.654 20 13.78 20z"
+                opacity="0.5"
+              />
+              <path strokeLinecap="round" d="m15.5 9.5l-5 5m0-5l5 5" />
+            </g>
+          </svg>
+        </Link>
+      </div>
+      <Suspense fallback={<FormSkeleton rows={2} />}>
+        <EditTemplate id={id} fallbackTemplate={template ?? undefined} />
+      </Suspense>
+    </>
   );
 }

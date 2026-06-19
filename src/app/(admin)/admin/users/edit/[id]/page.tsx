3192295b2f0c1b54ef08d855eec2b0303a18f512
raffo1234 <rhmcord@gmail.com>
2026-06-18 19:32:43 -0500

    feat: remove commented lines

--- src/app/(admin)/admin/users/edit/[id]/page.tsx
+++ src/app/(admin)/admin/users/edit/[id]/page.tsx
@@ -10,14 +10,10 @@ export default async function Page({ params }: { params: Params }) {
   console.log("Current user in EditUser page:", currentUser); // Debug log
   if (!id) return null;
   if (!currentUser) return <NoAccess />;
-  // if (!currentUser.roleId) return <NoAccess />;
-  // const [targetUser, allUsers] = await Promise.all([userFetcher(id), usersFetcher()]);
-
-  // if (!targetUser) return <NoAccess />;
 
   return (
     <FormSection title="Edit User" backUrl="/admin/users">
-      <EditUserContent userId={id}  />
+      <EditUserContent userId={id} />
     </FormSection>
   );
 }
